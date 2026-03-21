757fdd6b755c84c45367c5f000f67eee862ef8ab
sneh-cmd <snehadharadev011@gmail.com>
2026-03-21 12:58:36 +0530

    add

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:'/Ashish/',
+  base: process.env.NODE_ENV === 'production' ? '/Ashish/' : '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
